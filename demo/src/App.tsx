4bdca2bce386289ff69f6599f05a7ceef4f6dbea
Nicholaus Brabant <nicholaus.brabant@gmail.com>
2024-03-02 19:46:04 -0800

    Working demo app

---
+++ demo/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import { Canvas } from 'react-canvas-animate'
+
+function App() {
+  return (
+    <div className='App'>
+      <Canvas />
+    </div>
+  )
+}
+
+export default App
