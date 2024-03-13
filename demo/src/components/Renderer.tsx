3bb8b6168ee3c870f401ee208d21d4d3f5102442
Nicholaus Brabant <nicholaus.brabant@gmail.com>
2024-03-12 17:23:26 -0700

    Demo

--- demo/src/components/Renderer.tsx
+++ demo/src/components/Renderer.tsx
@@ -1,5 +1,12 @@
 import { useState, useRef } from 'react'
-import Canvas, { CanvasProps, Context2D } from 'react-canvas-animate'
+import Canvas, {
+  CanvasProps,
+  Context2D,
+  ImageLoader,
+  rgbAngle,
+} from 'react-canvas-animate'
+
+import logo from '../logo.svg'
 
 interface RendererProps extends CanvasProps {}
 
@@ -7,34 +14,58 @@ function Renderer({ ...props }: RendererProps) {
   const [offset, setOffset] = useState({ x: 0, y: 0 })
   const [isMouseDown, setIsMouseDown] = useState(false)
   const [grabStart, setGrabStart] = useState({ x: 0, y: 0 })
+  const [zoom, setZoom] = useState(4)
+  const zoomRef = useRef(4)
+  const angleRef = useRef(0)
+
+  const angularSpeed = Math.PI / 10
 
   const offsetRef = useRef({ x: 0, y: 0 })
   const colorRef = useRef('#00d5ff')
+  const imageRef = useRef<ImageLoader>()
+
+  const init = (ctx: Context2D) => {
+    imageRef.current = new ImageLoader([logo])
+  }
 
   const render = (ctx: Context2D) => {
     const { width, height } = ctx.canvas
+    ctx.save()
     ctx.fillStyle = colorRef.current
+    ctx.shadowBlur = 5 * zoomRef.current
+    ctx.shadowColor = rgbAngle(angleRef.current * 360)
+    // ctx.shadowOffsetX = 5 * zoomRef.current
+    // ctx.shadowOffsetY = 5 * zoomRef.current
     ctx.clearRect(0, 0, width, height)
-    ctx.fillRect(
-      width / 2 - 25 + offsetRef.current.x,
-      height / 2 - 25 + offsetRef.current.y,
-      50,
-      50,
+    const img = imageRef.current?.getImage(logo) || new Image()
+    const sizeX = img.width * zoomRef.current
+    const sizeY = img.height * zoomRef.current
+    ctx.translate(
+      width / 2 + offsetRef.current.x,
+      height / 2 + offsetRef.current.y,
     )
+    ctx.rotate(angleRef.current)
+    ctx.drawImage(img, -sizeX / 2, -sizeY / 2, sizeX, sizeY)
+    // ctx.fillRect(-size / 2, -size / 2, size, size)
+    ctx.restore()
+  }
+
+  const update = (ctx: Context2D, deltaTime: number) => {
+    angleRef.current += angularSpeed * (deltaTime / 1000)
   }
 
   const handleMouseDown = (event: MouseEvent) => {
     setIsMouseDown(true)
-    setGrabStart({ x: event.clientX, y: event.clientY })
+    setGrabStart({ x: event.offsetX, y: event.offsetY })
   }
 
   const handleMouseMove = (event: MouseEvent) => {
     if (isMouseDown) {
-      const dx = event.clientX - grabStart.x
-      const dy = event.clientY - grabStart.y
+      const dx = event.offsetX - grabStart.x
+      const dy = event.offsetY - grabStart.y
       setOffset({ x: offset.x + dx, y: offset.y + dy })
       offsetRef.current = offset
-      setGrabStart({ x: event.clientX, y: event.clientY })
+      setGrabStart({ x: event.offsetX, y: event.offsetY })
     }
   }
 
@@ -49,6 +80,13 @@ function Renderer({ ...props }: RendererProps) {
       case 'mousemove':
         handleMouseMove(event as MouseEvent)
         break
+      case 'wheel':
+        const e = event as WheelEvent
+        let newZoom = zoom + e.deltaY * -0.01
+        newZoom = Math.max(0.5, Math.min(10, newZoom))
+        zoomRef.current = newZoom
+        setZoom(newZoom)
+        break
       default:
         break
     }
@@ -57,18 +95,21 @@ function Renderer({ ...props }: RendererProps) {
   return (
     <>
       <Canvas
+        init={init}
         render={render}
+        update={update}
         gridOffset={offset}
-        gridSize={40}
-        events={{
+        gridSize={20 * zoom}
+        documentEvents={{
           handleEvent,
-          eventTypes: ['mousedown', 'mouseup', 'mousemove'],
+          eventTypes: ['mousedown', 'mouseup', 'mousemove', 'wheel'],
         }}
         style={{ cursor: isMouseDown ? 'grabbing' : 'grab' }}
+        fullscreen
         {...props}
       />
-      <button onClick={() => (colorRef.current = '#00d5ff')}>Blue</button>
-      <button onClick={() => (colorRef.current = '#ffd500')}>Yellow</button>
+      {/* <button onClick={() => (colorRef.current = '#00d5ff')}>Blue</button>
+      <button onClick={() => (colorRef.current = '#ffd500')}>Yellow</button> */}
     </>
   )
 }
