933c5f2b7aa23dc558b5d11eeb886d19177c8c01
Nicholaus Brabant <nick@Nicholauss-MacBook-Pro.local>
2024-03-13 19:57:33 -0700

    Refactor demo

--- demo/src/App.tsx
+++ demo/src/App.tsx
@@ -1,28 +1,5 @@
-// import { useRef } from 'react'
-// import GLRenderer from './components/GLRenderer'
-// import { WebGL } from 'react-canvas-animate'
-// import { RenderableObjectManager as ObjectManager } from './objects/managers/RenderableObjectManager'
-// import { ClearScreen } from './objects/sandbox/ClearScreen'
+import { Renderer } from './components'
 
-// function App() {
-//   const objects = useRef<ObjectManager>()
-
-//   const init = (gl: WebGL) => {
-//     objects.current = new ObjectManager(gl)
-//     objects.current.create(ClearScreen, {})
-//   }
-
-//   const render = (gl: WebGL) => {
-//     objects.current?.render()
-//   }
-
-//   return <GLRenderer postInit={init} render={render} fullscreen />
-// }
-
-import Renderer from './components/Renderer'
-
-function App() {
-  return <Renderer width={1024} height={768} />
+export default function App() {
+  return <Renderer fullscreen />
 }
-
-export default App
--- demo/src/objects/base/RenderableObject2D.ts
+++
@@ -1,7 +0,0 @@
-import { CanvasObject, WebGL } from 'react-canvas-animate'
-
-export class RenderableObject2D extends CanvasObject.Base<WebGL> {
-  render() {
-    // this.context
-  }
-}
